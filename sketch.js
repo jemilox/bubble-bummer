5a26d55d511d0c2bac66ed11b335bc7a4d287577
jemilox <jemilox@gmail.com>
2017-07-17 18:27:46 -0500

    ball man and bubble

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
 function setup() {
-
+ createCanvas(1920*.8, 1080*.8);
 }
+
+var x = 50;
+var y = 50;
+var bubbleStartX = 200;
+var bubbleStartY =  0;
+var bubbleArray = [];
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  if (keyIsDown(LEFT_ARROW))
+    x-=5;
+
+  if (keyIsDown(RIGHT_ARROW))
+    x+=5;
+
+  if (keyIsDown(UP_ARROW))
+    y-=5;
+
+  if (keyIsDown(DOWN_ARROW))
+    y+=5;
+
+  background(51);
+  ellipse(x, y, 80, 80);
+  //bubbles
+  createBubble();
+  bubbleStartY ++;
 }
+
+var createBubble = function(){
+  ellipse(bubbleStartX, bubbleStartY - 100, 200, 200);
+};
